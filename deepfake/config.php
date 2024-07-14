246ad98d80fc08835c3bbaf842eb67c0168096b6
yohanachristela20 <97132324+yohanachristela20@users.noreply.github.com>
2024-07-14 09:26:24 +0700

    Update config.php

--- deepfake/config.php
+++ deepfake/config.php
@@ -3,13 +3,14 @@ date_default_timezone_set("Asia/Jakarta");
 error_reporting(0);
 
 // Database connection details
-$host 	= "127.0.0.1"; // host server
-$user 	= "root";  // username server
-$pass 	= ""; // password server, kalau pakai xampp kosongin saja
-$dbname = "db_deepfake"; // nama database anda
+$host 	= "localhost"; // host server
+$port   = "3306";
+$user 	= "scholar3_deepfake";  // username server
+$pass 	= "deepfakekel2"; // password server, kalau pakai xampp kosongin saja
+$dbname = "scholar3_db_deepfake"; // nama database anda
 
 try {
-    $config = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $config = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $pass);
     $config->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo 'sukses';
 } catch (PDOException $e) {
